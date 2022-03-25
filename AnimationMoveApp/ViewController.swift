6b1e3fc783323a0976bcc7f4bebaf49554dec406
Ishtiaque Ahmed <iahmed85@my.centennialcollege.ca>
2022-03-25 15:32:53 -0400

    final commit

--- AnimationMoveApp/ViewController.swift
+++ AnimationMoveApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIView.animate(withDuration: 2.0) {
+            self.myLabel.center.y += self.view.bounds.height
+            self.myTextField.center.y += self.view.bounds.height
+            self.myImageView.center.y += self.view.bounds.height
+        }
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        myLabel.center.y -= view.bounds.height
+        myTextField.center.y -= view.bounds.height
+        myImageView.center.y -= view.bounds.height
+    }
 
 }
-
